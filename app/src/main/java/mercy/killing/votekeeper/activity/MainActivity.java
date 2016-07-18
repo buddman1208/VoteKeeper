1aaa8a1135ab1d9be60407c309c9095f91783890
Junseok Oh <kotohana5706@edcan.kr>
2016-07-19 01:23:57 +0900

    Implement SplashActivity

---
+++ app/src/main/java/mercy/killing/votekeeper/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package mercy.killing.votekeeper.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mercy.killing.votekeeper.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
