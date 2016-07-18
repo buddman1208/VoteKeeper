9c22e85312c813b9e5497e572bbd8e6426ab268f
Junseok Oh <kotohana5706@edcan.kr>
2016-07-19 01:30:55 +0900

    Implement GroupAddActivity

---
+++ app/src/main/java/mercy/killing/votekeeper/activity/GroupAddActivity.java
@@ -0,0 +1,13 @@
+package mercy.killing.votekeeper.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GroupAddActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_add);
+    }
+}
