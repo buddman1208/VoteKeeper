b8a0a1e87791709766f6ef67a8c93ad45c7a8cfe
Junseok Oh <kotohana5706@edcan.kr>
2016-07-19 01:28:34 +0900

    Implement VoteRegisterActivity

--- app/src/main/java/mercy/killing/votekeeper/activity/VoteRegisterActivity.java
+++ app/src/main/java/mercy/killing/votekeeper/activity/VoteRegisterActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import mercy.killing.votekeeper.R;
 
-public class GroupSelectActivity extends AppCompatActivity {
+public class VoteRegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group_select);
+        setContentView(R.layout.activity_vote_register);
     }
 }
