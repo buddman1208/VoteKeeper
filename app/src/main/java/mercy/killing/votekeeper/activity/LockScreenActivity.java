2ab612bcc0903df551f01bf8fcd551b627e28648
Junseok Oh <kotohana5706@edcan.kr>
2016-07-19 01:29:16 +0900

    Implement LockScreenActivity

---
+++ app/src/main/java/mercy/killing/votekeeper/activity/LockScreenActivity.java
@@ -0,0 +1,15 @@
+package mercy.killing.votekeeper.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mercy.killing.votekeeper.R;
+
+public class LockScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock_screen);
+    }
+}
